c6b37271a4c0e8fb6d54ebf55103fb40c80dea36
Wolfgang Egger <w.egger@framlin.com>
2022-08-15 16:46:59 +0200

    FRAT-17 write Custom Test Driver for electron and jest

---
+++ test/postit/app.test.js
@@ -0,0 +1,29 @@
+const electronPath = require('electron')
+const {TestDriver} = require('./testDriver')
+
+const app = new TestDriver({
+    path: electronPath,
+    args: ['./ui/electron/main.js'],
+    env: {
+        NODE_ENV: 'test'
+    }
+})
+
+beforeEach(async () => {
+    await app.isReady;
+})
+
+test('electron Test Driver', async () => {
+
+    let result = await app.rpc('a_test');
+
+    expect(result).toBe(42);
+})
+
+afterEach(async () => {
+    await app.stop();
+})
+
+
+
+
--- ui/electron/main.js
+++ ui/electron/main.js
@@ -31,3 +31,36 @@ app.on('window-all-closed', () => {
         app.quit();
     }
 });
+
+
+//================================    TEST     ==================================
+const METHODS = {
+    isReady () {
+        // do any setup needed
+        return true
+    },
+    // define your RPC-able methods here
+    a_test () {
+        return 42;
+    }
+}
+
+const onMessage = async ({ msgId, cmd, args }) => {
+    let method = METHODS[cmd]
+    if (!method) method = () => new Error('Invalid method: ' + cmd)
+    try {
+        const resolve = await method(...args)
+        process.send({ msgId, resolve })
+    } catch (err) {
+        const reject = {
+            message: err.message,
+            stack: err.stack,
+            name: err.name
+        }
+        process.send({ msgId, reject })
+    }
+}
+
+if (process.env.APP_TEST_DRIVER) {
+    process.on('message', onMessage)
+}
