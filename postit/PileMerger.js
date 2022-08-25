d3832b77b188cc49649bcd26aa212af26b89abc8
framlin <w.egger@framlin.com>
2022-08-25 14:56:43 +0200

    FRAT-51 add comfort-test-function and more tests

--- postit/PileMerger.js
+++ postit/PileMerger.js
@@ -1,26 +1,43 @@
 const Pile = require("./Pile");
 
+function find_same_postit(postits, postit) {
+    return postits.find((pattern) => pattern.is_same_as(postit))
+}
+
+function find_equal_postit(postits, postit) {
+    return postits.find((pattern) => pattern.is_equal_with(postit))
+}
+
+
 class PileMerger {
     merge(pile_1, pile_2) {
         let result;
-        let elems_1 = pile_1.all;
-        let elems_2 = pile_2.all;
-        if (elems_1.length === 0) {
-            result = new Pile(elems_2);
+        let pile_1_items = pile_1.all;
+        let pile_2_items = pile_2.all;
+        let result_items = [];
+        if (pile_1_items.length === 0) {
+            result = new Pile(pile_2_items);
         } else {
-            for (let elem2 of elems_2) {
-                let match = false;
-                for (let elem1 of elems_1) {
-                    if (elem2.is_equal_with(elem1)){
-                        match = true;
-                        break;
-                    }
-                }
-                if (!match) elems_1.push(elem2);
-                // if (!elem.is_equal_with(elems_1[0]))
-                //     elems_1.push(elem);
+            for (let pile_1_postit of pile_1_items) {
+                result_items.push(pile_1_postit);
             }
-            result = new Pile(elems_1);
+            for (let pile_2_postit of pile_2_items) {
+                let same_postit_in_result = find_same_postit(result_items, pile_2_postit);
+                if (same_postit_in_result){
+                    let equal_postit_in_result = find_equal_postit(result_items, pile_2_postit);
+                    if (!equal_postit_in_result) {
+                        let last_update_in_result =  same_postit_in_result.last_update;
+                        let last_update_of_pile_2_postit = pile_2_postit.last_update;
+                        if (last_update_in_result < last_update_of_pile_2_postit) {
+                           let index = result_items.indexOf(same_postit_in_result);
+                            result_items[index] = pile_2_postit
+                        }
+                    }
+                } else {
+                    result_items.push(pile_2_postit);
+                }
+            }
+            result = new Pile(result_items);
         }
         return result;
     }
--- test/postit/PileMerger.test.js
+++ test/postit/PileMerger.test.js
@@ -13,8 +13,13 @@ _	    1,2	    1,2
 1,2	    2,3	    1,2,3
 1*	    1	    1*
 1	    1*	    1*
-1*,2	1,2	    1*,2
+1*	    1,2	    1*,2 #
+1,2	    1*	    1*,2
 1,2	    1*,2	1*,2
+1*,2	1,2	    1*,2
+1*,2*	1	    1*,2*
+1	    1*,2*	1*,2*
+1*,2	1,2*	1*,2*
 1*,2*	1,2	    1*,2*
 1,2	    1*,2*	1*,2*
 1,2*	2,3	    1,2*,3
@@ -31,19 +36,165 @@ function clone_postit(postit) {
     return Postit.from_JSON(json);
 }
 
-async function create_two_different_postits() {
-    let postit_1 = new Postit('1');
-    await new Promise((r) => setTimeout(r, 2));
-    let postit_2 = new Postit('2');
-    return {postit_1, postit_2}
+async function test_pile_merging(pile_1_items, pile_2_items, expected_items) {
+    let result = [];
+    let {pile_1, pile_2} = await create_piles(pile_1_items, pile_2_items);
+    let merged_pile =  pile_merger.merge(pile_1, pile_2);
+    return merged_pile;
+}
+async function create_piles(pile_1_items, pile_2_items) {
+    function clone_from(pile_postits, item) {
+        let original_postit = pile_postits.find((postit) => postit.text === item);
+        let cloned_postit = clone_postit(original_postit);
+        return {original_postit, cloned_postit};
+    }
+    let pile_1_postits = [];
+    let pile_1_item_list = pile_1_items.split(',');
+    for (let item of pile_1_item_list) {
+        if (item === '_') break;
+        pile_1_postits.push(new Postit(item));
+        await new Promise((r) => setTimeout(r, 2));
+    }
+    let pile_1 = new Pile(pile_1_postits);
+
+    let pile_2_postits = [];
+    let pil_2_item_list = pile_2_items.split(',');
+    for (let item of pil_2_item_list) {
+        if (item === '_') break;
+        if (pile_1_item_list.includes(item)) {
+            let {cloned_postit} = clone_from(pile_1_postits, item);
+            pile_2_postits.push(cloned_postit);
+        } else if (pile_1_item_list.includes(item+"*")) {
+            let {original_postit, cloned_postit} = clone_from(pile_1_postits, item+"*");
+            cloned_postit.text = item;
+            await new Promise((r) => setTimeout(r, 2));
+            original_postit.text = item+"*";
+            pile_2_postits.push(cloned_postit);
+        } else {
+            if (item.includes('*')) {
+                let {cloned_postit} = clone_from(pile_1_postits, item.split('*')[0]);
+                cloned_postit.text = item;
+                pile_2_postits.push(cloned_postit);
+            } else {
+                pile_2_postits.push(new Postit(item));
+
+            }
+        }
+        await new Promise((r) => setTimeout(r, 2));
+    }
+    let pile_2 = new Pile(pile_2_postits);
+    return {pile_1, pile_2}
 }
 
 
-function merge_piles(items_1, items_2) {
-    let pile_1 = new Pile(items_1);
-    let pile_2 = new Pile(items_2);
-    return pile_merger.merge(pile_1, pile_2);
-}
+describe('test_pile_merging-function', () => {
+    describe('create_piles', () => {
+        test('_	    _', async () => {
+            let {pile_1, pile_2} = await create_piles("_", "_");
+            expect(pile_1.all.length + pile_2.all.length).toBe(0);
+        });
+
+        test('1	    _', async () => {
+            let {pile_1, pile_2} = await create_piles("1", "_");
+            expect(pile_1.all.length + pile_2.all.length).toBe(1);
+            expect(pile_1.all[0].text).toBe("1");
+        });
+
+        test('_	    1', async () => {
+            let {pile_1, pile_2} = await create_piles("_", "1");
+            expect(pile_1.all.length + pile_2.all.length).toBe(1);
+            expect(pile_2.all[0].text).toBe("1");
+        });
+
+        test('1	    1', async () => {
+            let {pile_1, pile_2} = await create_piles("1", "1");
+            expect(pile_1.all.length + pile_2.all.length).toBe(2);
+            expect(pile_1.all[0].is_same_as(pile_2.all[0])).toBe(true);
+        });
+
+        test('1	    2', async () => {
+            let {pile_1, pile_2} = await create_piles("1", "2");
+            expect(pile_1.all.length + pile_2.all.length).toBe(2);
+            expect(pile_1.all[0].is_same_as(pile_2.all[0])).toBe(false);
+        });
+
+        test('2	    1', async () => {
+            let {pile_1, pile_2} = await create_piles("2", "1");
+            expect(pile_1.all.length + pile_2.all.length).toBe(2);
+            expect(pile_1.all[0].is_same_as(pile_2.all[0])).toBe(false);
+        });
+
+        test('1	    1,2', async () => {
+            let {pile_1, pile_2} = await create_piles("1", "1,2");
+            expect(pile_1.all.length + pile_2.all.length).toBe(3);
+            expect(pile_1.all[0].is_equal_with(pile_2.all[0])).toBe(true);
+            expect(pile_1.all[0].is_equal_with(pile_2.all[1])).toBe(false);
+        });
+
+        test('1	    1*', async () => {
+            let {pile_1, pile_2} = await create_piles("1", "1*");
+            expect(pile_1.all.length + pile_2.all.length).toBe(2);
+            expect(pile_1.all[0].is_same_as(pile_2.all[0])).toBe(true);
+            expect(pile_1.all[0].is_equal_with(pile_2.all[0])).toBe(false);
+        });
+
+        test('1*	    1', async () => {
+            let {pile_1, pile_2} = await create_piles("1*", "1");
+            expect(pile_1.all.length + pile_2.all.length).toBe(2);
+            expect(pile_1.all[0].is_same_as(pile_2.all[0])).toBe(true);
+            expect(pile_1.all[0].is_equal_with(pile_2.all[0])).toBe(false);
+        });
+
+        test('1,2	    1,2', async () => {
+            let {pile_1, pile_2} = await create_piles("1,2", "1,2");
+            expect(pile_1.all.length + pile_2.all.length).toBe(4);
+            expect(pile_1.all[0].is_equal_with(pile_2.all[0])).toBe(true);
+            expect(pile_1.all[1].is_equal_with(pile_2.all[1])).toBe(true);
+        });
+
+        test('1,2*	    1,2', async () => {
+            let {pile_1, pile_2} = await create_piles("1,2*", "1,2");
+            expect(pile_1.all.length + pile_2.all.length).toBe(4);
+            expect(pile_1.all[0].is_equal_with(pile_2.all[0])).toBe(true);
+            expect(pile_1.all[1].is_same_as(pile_2.all[1])).toBe(true);
+            expect(pile_1.all[1].is_equal_with(pile_2.all[1])).toBe(false);
+            expect(pile_1.all[1].last_update).toBeGreaterThan(pile_2.all[1].last_update);
+        });
+
+        test('1,2	    1,2*', async () => {
+            let {pile_1, pile_2} = await create_piles("1,2", "1,2*");
+            expect(pile_1.all.length + pile_2.all.length).toBe(4);
+            expect(pile_1.all[0].is_equal_with(pile_2.all[0])).toBe(true);
+            expect(pile_1.all[1].is_same_as(pile_2.all[1])).toBe(true);
+            expect(pile_1.all[1].is_equal_with(pile_2.all[1])).toBe(false);
+            expect(pile_1.all[1].last_update).toBeLessThan(pile_2.all[1].last_update);
+        });
+
+        test('1,2	    1,2,3', async () => {
+            let {pile_1, pile_2} = await create_piles("1,2", "1,2,3");
+            expect(pile_1.all.length + pile_2.all.length).toBe(5);
+            expect(pile_1.all[0].is_equal_with(pile_2.all[0])).toBe(true);
+            expect(pile_1.all[1].is_equal_with(pile_2.all[1])).toBe(true);
+            expect(pile_2.all[2].text).toBe("3");
+        });
+
+        test('1,2,3,4*,5,6*,8,9       1*,4,5,6,8,9*', async () => {
+            let {pile_1, pile_2} = await create_piles("1,2,3,4*,5,6*,8,9", "1*,4,5,6,8,9*");
+            expect(pile_1.all.length + pile_2.all.length).toBe(14);
+            expect(pile_1.all[0].is_same_as(pile_2.all[0])).toBe(true);
+            expect(pile_1.all[0].is_equal_with(pile_2.all[0])).toBe(false);
+            expect(pile_1.all[3].is_same_as(pile_2.all[1])).toBe(true);
+            expect(pile_1.all[3].is_equal_with(pile_2.all[1])).toBe(false);
+            expect(pile_1.all[4].is_equal_with(pile_2.all[2])).toBe(true);
+            expect(pile_1.all[5].is_same_as(pile_2.all[3])).toBe(true);
+            expect(pile_1.all[5].is_equal_with(pile_2.all[3])).toBe(false);
+            expect(pile_1.all[6].is_equal_with(pile_2.all[4])).toBe(true);
+            expect(pile_1.all[7].is_same_as(pile_2.all[5])).toBe(true);
+            expect(pile_1.all[7].is_equal_with(pile_2.all[5])).toBe(false);
+        })
+    });
+});
+
 beforeEach(() => {
     pile_merger = new PileMerger();
 })
@@ -52,64 +203,103 @@ it('can be created', () => {
     expect(pile_merger).toBeInstanceOf(PileMerger);
 });
 
-test ('_	    _	    _', () => {
-    let merged_pile = merge_piles([], [])
+test ('_	    _	    _', async () => {
+    let merged_pile = await test_pile_merging('_', '_')
     expect(merged_pile.all).toStrictEqual([]);
 });
 
-test('1	    _	    1', () => {
-    let postit = new Postit();
-    let merged_pile = merge_piles([postit], [])
+test('1	    _	    1', async () => {
+    let merged_pile = await test_pile_merging('1', '_')
     expect(merged_pile.all.length).toBe(1);
-    expect(merged_pile.all[0] === postit).toBe(true);
+    expect(merged_pile.all[0].text).toBe('1');
 });
 
-test('_	    1	    1', () => {
-    let postit = new Postit();
-    let merged_pile = merge_piles([], [postit])
+test('_	    1	    1', async () => {
+    let merged_pile = await test_pile_merging('_', '1')
     expect(merged_pile.all.length).toBe(1);
-    expect(merged_pile.all[0] === postit).toBe(true);
+    expect(merged_pile.all[0].text).toBe('1');
 });
 
-test('1	    1	    1', () => {
-    let postit_1 = new Postit('text');
-    let postit_2 = clone_postit(postit_1);
-    let merged_pile = merge_piles([postit_1], [postit_2])
+test('1	    1	    1', async () => {
+    let merged_pile = await test_pile_merging('1', '1')
     expect(merged_pile.all.length).toBe(1);
-    expect(merged_pile.all[0] === postit_1).toBe(true);
+    expect(merged_pile.all[0].text).toBe('1');
 })
 
 test('1,2	    _	    1,2', async () => {
-    const {postit_1, postit_2} = await create_two_different_postits();
-    let merged_pile = merge_piles([postit_1, postit_2], [])
+    let merged_pile = await test_pile_merging('1,2', '_')
     expect(merged_pile.all.length).toBe(2);
-    expect(merged_pile.all[0] === postit_1).toBe(true);
+    expect(merged_pile.all[0].text).toBe('1');
+    expect(merged_pile.all[1].text).toBe('2');
 });
 
 test('_	    1,2	    1,2', async () => {
-    const {postit_1, postit_2} = await create_two_different_postits();
-    let merged_pile = merge_piles([], [postit_1, postit_2])
+    let merged_pile = await test_pile_merging('_', '1,2')
     expect(merged_pile.all.length).toBe(2);
-    expect(merged_pile.all[0] === postit_1).toBe(true);
+    expect(merged_pile.all[0].text).toBe('1');
+    expect(merged_pile.all[1].text).toBe('2');
 });
 
 test('1,2	    1	    1,2', async () => {
-    const {postit_1, postit_2} = await create_two_different_postits();
-    let merged_pile = merge_piles([postit_1, postit_2], [postit_1])
+    let merged_pile = await test_pile_merging('1,2', '1')
     expect(merged_pile.all.length).toBe(2);
-    expect(merged_pile.all[0] === postit_1).toBe(true);
+    expect(merged_pile.all[0].text).toBe('1');
+    expect(merged_pile.all[1].text).toBe('2');
 });
 
 test('1	    1,2	    1,2', async () => {
-    const {postit_1, postit_2} = await create_two_different_postits();
-    let merged_pile = merge_piles([postit_1], [postit_1, postit_2])
+    let merged_pile = await test_pile_merging('1', '1,2')
     expect(merged_pile.all.length).toBe(2);
-    expect(merged_pile.all[0] === postit_1).toBe(true);
+    expect(merged_pile.all[0].text).toBe('1');
+    expect(merged_pile.all[1].text).toBe('2');
 });
 
 test('1,2	    1,2	    1,2', async () => {
-    const {postit_1, postit_2} = await create_two_different_postits();
-    let merged_pile = merge_piles([postit_1, postit_2], [postit_1, postit_2])
+    let merged_pile = await test_pile_merging('1,2', '1,2')
     expect(merged_pile.all.length).toBe(2);
-    expect(merged_pile.all[0] === postit_1).toBe(true);
+    expect(merged_pile.all[0].text).toBe('1');
+    expect(merged_pile.all[1].text).toBe('2');
+});
+
+test('1,2    3    1,2,3', async () => {
+    let merged_pile = await test_pile_merging('1,2', '3')
+    expect(merged_pile.all.length).toBe(3);
+    expect(merged_pile.all[0].text).toBe('1');
+    expect(merged_pile.all[1].text).toBe('2');
+    expect(merged_pile.all[2].text).toBe('3');
+});
+
+test('1    2,3    1,2,3', async () => {
+    let merged_pile = await test_pile_merging('1', '2,3')
+    expect(merged_pile.all.length).toBe(3);
+    expect(merged_pile.all[0].text).toBe('1');
+    expect(merged_pile.all[1].text).toBe('2');
+    expect(merged_pile.all[2].text).toBe('3');
+});
+
+test('1,2    2,3    1,2,3', async () => {
+    let merged_pile = await test_pile_merging('1,2', '2,3')
+    expect(merged_pile.all.length).toBe(3);
+    expect(merged_pile.all[0].text).toBe('1');
+    expect(merged_pile.all[1].text).toBe('2');
+    expect(merged_pile.all[2].text).toBe('3');
+});
+
+test('1*    1    1*', async () => {
+    let merged_pile = await test_pile_merging('1*', '1')
+    expect(merged_pile.all.length).toBe(1);
+    expect(merged_pile.all[0].text).toBe('1*');
+});
+
+test('1    1*    1*', async () => {
+    let merged_pile = await test_pile_merging('1', '1*')
+    expect(merged_pile.all.length).toBe(1);
+    expect(merged_pile.all[0].text).toBe('1*');
+});
+
+test('1*    1,2    1*,2', async () => {
+    let merged_pile = await test_pile_merging('1*', '1,2')
+    expect(merged_pile.all.length).toBe(2);
+    expect(merged_pile.all[0].text).toBe('1*');
+    expect(merged_pile.all[1].text).toBe('2');
 });
